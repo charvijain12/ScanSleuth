760c798d83bc9ede6bc720b88d2d227e2980fa36
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2023-11-14 08:29:47 +0530

    Add files via upload

---
+++ DenseNet Model/Xray Analysis Densenet Model.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Applying DenseNet on NIH Chest dataset
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from glob import glob
+import os
+import matplotlib.gridspec as gridspec
+import matplotlib.ticker as ticker
+sns.set_style('whitegrid')
+get_ipython().run_line_magic('matplotlib', 'inline')
+import warnings
+warnings.filterwarnings('ignore')
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import Dense
+from keras.optimizers import Adam
+#from generator import DataGenerator
+import keras
+
+
+# <h1> Creating data for model </h1>
+
+# In[2]:
+
+
+data = pd.read_csv("C:\\Users\\Karan\\Downloads\\archive\\Data_Entry_2017.csv")
+data = data[data['Patient Age']<100] #removing datapoints which having age greater than 100
+data_image_paths = {os.path.basename(x): x for x in 
+                   glob(os.path.join('..', 'input', 'images*', '*', '*.png'))}
+print('Scans found:', len(data_image_paths), ', Total Headers', data.shape[0])
+data['path'] = data['Image Index'].map(data_image_paths.get)
+data['Patient Age'] = data['Patient Age'].map(lambda x: int(x))
+data.sample(3)
+
+
+# In[3]:
+
+
+data['Finding Labels'] = data['Finding Labels'].map(lambda x: x.replace('No Finding', ''))
+from itertools import chain
+all_labels = np.unique(list(chain(*data['Finding Labels'].map(lambda x: x.split('|')).tolist())))
+all_labels = [x for x in all_labels if len(x)>0]
+print('All Labels ({}): {}'.format(len(all_labels), all_labels))
+for c_label in all_labels:
+    if len(c_label)>1: # leave out empty labels
+        data[c_label] = data['Finding Labels'].map(lambda finding: 1.0 if c_label in finding else 0)
+data.sample(3)
+
+
+# In[4]:
+
+
+# keep at least 1000 cases
+MIN_CASES = 1000
+all_labels = [c_label for c_label in all_labels if data[c_label].sum()>MIN_CASES]
+print('Clean Labels ({})'.format(len(all_labels)), 
+      [(c_label,int(data[c_label].sum())) for c_label in all_labels])
+
+
+# In[5]:
+
+
+# since the dataset is very unbiased, we can resample it to be a more reasonable collection
+# weight is 0.04 + number of findings
+sample_weights = data['Finding Labels'].map(lambda x: len(x.split('|')) if len(x)>0 else 0).values + 4e-2
+sample_weights /= sample_weights.sum()
+data = data.sample(40000, weights=sample_weights)
+
+label_counts = data['Finding Labels'].value_counts()[:15]
+fig, ax1 = plt.subplots(1,1,figsize = (12, 8))
+ax1.bar(np.arange(len(label_counts))+0.5, label_counts)
+ax1.set_xticks(np.arange(len(label_counts))+0.5)
+_ = ax1.set_xticklabels(label_counts.index, rotation = 90)
+
+
+# In[6]:
+
+
+# creating vector of diseases
+data['disease_vec'] = data.apply(lambda x: [x[all_labels].values], 1).map(lambda x: x[0])
+
+
+# In[8]:
+
+
+from sklearn.model_selection import train_test_split
+train_df, test_df = train_test_split(data, 
+                                   test_size = 0.20, 
+                                   random_state = 2018,
+                                   stratify = data['Finding Labels'].map(lambda x: x[:4]))
+print('train', train_df.shape[0], 'test', test_df.shape[0])
+
+
+# In[9]:
+
+
+train_df, valid_df = train_test_split(train_df, 
+                                   test_size = 0.10, 
+                                   random_state = 2018,
+                                   stratify = train_df['Finding Labels'].map(lambda x: x[:4]))
+print('train', train_df.shape[0], 'valid', valid_df.shape[0])
+
+
+# In[10]:
+
+
+def flow_from_dataframe(img_data_gen, in_df, path_col, y_col, **dflow_args):
+    base_dir = os.path.dirname(in_df[path_col].values[0])
+    print('## Ignore next message from keras, values are replaced anyways')
+    df_gen = img_data_gen.flow_from_directory(base_dir, 
+                                     class_mode = 'sparse',
+                                    **dflow_args)
+    df_gen.filenames = in_df[path_col].values
+    df_gen.classes = np.stack(in_df[y_col].values)
+    df_gen.samples = in_df.shape[0]
+    df_gen.n = in_df.shape[0]
+    df_gen._set_index_array()
+    df_gen.directory = '' # since we have the full path
+    print('Reinserting dataframe: {} images'.format(in_df.shape[0]))
+    return df_gen
+
+
+
+
+# In[11]:
+
+
+from keras.applications.densenet import DenseNet121, preprocess_input
+
+
+# In[12]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+from PIL import Image
+IMG_SIZE = (224, 224) # slightly smaller than vgg16 normally expects
+core_idg_dense = ImageDataGenerator()
+
+
+# In[13]:
+
+
+train_gen = flow_from_dataframe(core_idg_dense, train_df, 
+                             path_col = 'path',
+                            y_col = 'disease_vec', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 16)
+
+valid_gen = flow_from_dataframe(core_idg_dense, valid_df, 
+                             path_col = 'path',
+                            y_col = 'disease_vec', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 32) # we can use much larger batches for evaluation
+# used a fixed dataset for evaluating the algorithm
+test_X, test_Y = next(flow_from_dataframe(core_idg_dense, 
+                               test_df, 
+                             path_col = 'path',
+                            y_col = 'disease_vec', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 8000)) # one big batch
+# used a fixed dataset for final evaluation
+
+
+# In[14]:
+
+
+t_x, t_y = next(train_gen)
+fig, m_axs = plt.subplots(4, 4, figsize = (16, 16))
+for (c_x, c_y, c_ax) in zip(t_x, t_y, m_axs.flatten()):
+    c_ax.imshow(c_x[:,:,0])
+    c_ax.set_title(', '.join([n_class for n_class, n_score in zip(all_labels, c_y) 
+                             if n_score>0.5]))
+    c_ax.axis('off')
+
+
+# ### DenseNet
+
+# In[16]:
+
+
+# dense net model
+img_in = Input(t_x.shape[1:])              #input of model 
+model = DenseNet121(include_top= False , # remove  the 3 fully-connected layers at the top of the network
+                weights='imagenet',      # pre train weight 
+                input_tensor= img_in, 
+                input_shape= t_x.shape[1:],
+                pooling ='avg') 
+
+x = model.output  
+predictions = Dense(len(all_labels), activation="sigmoid", name="predictions")(x)    # fuly connected layer for predict class 
+model = Model(inputs=img_in, outputs=predictions)
+
+
+# In[17]:
+
+
+optimizer = Adam(lr=0.001)
+model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=[keras.metrics.binary_accuracy])
+
+
+# ##### training first 20 epochs
+
+# In[18]:
+
+
+history = model.fit_generator(train_gen, 
+                                  steps_per_epoch=100,
+                                  validation_data = valid_gen, 
+                                  epochs = 20)
+
+
+# In[19]:
+
+
+y_pred = model.predict(test_X)
+
+
+# In[20]:
+
+
+# look at how often the algorithm predicts certain diagnoses 
+for c_label, p_count, t_count in zip(all_labels, 
+                                     100*np.mean(y_pred,0), 
+                                     100*np.mean(test_Y,0)):
+    print('%s: actual: %2.2f%%, predicted: %2.2f%%' % (c_label, t_count, p_count))
+
+
+# In[21]:
+
+
+from sklearn.metrics import roc_curve, auc
+fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+for (idx, c_label) in enumerate(all_labels):
+    fpr, tpr, thresholds = roc_curve(test_Y[:,idx].astype(int), y_pred[:,idx])
+    c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % (c_label, auc(fpr, tpr)))
+c_ax.legend()
+c_ax.set_xlabel('False Positive Rate')
+c_ax.set_ylabel('True Positive Rate')
+fig.savefig('trained_net.png')
+
+
+# In[22]:
+
+
+from sklearn.metrics import roc_auc_score
+roc_auc_score(test_Y.astype(int), y_pred)
+
+
+# ##### training next 20 epochs
+
+# In[23]:
+
+
+history = model.fit_generator(train_gen, 
+                                  steps_per_epoch=100,
+                                  validation_data = valid_gen, 
+                                  epochs = 20)
+
+
+# In[24]:
+
+
+y_pred = model.predict(test_X)
+
+
+# In[25]:
+
+
+# look at how often the algorithm predicts certain diagnoses 
+for c_label, p_count, t_count in zip(all_labels, 
+                                     100*np.mean(y_pred,0), 
+                                     100*np.mean(test_Y,0)):
+    print('%s: actual: %2.2f%%, predicted: %2.2f%%' % (c_label, t_count, p_count))
+
+
+# In[26]:
+
+
+from sklearn.metrics import roc_curve, auc
+fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+for (idx, c_label) in enumerate(all_labels):
+    fpr, tpr, thresholds = roc_curve(test_Y[:,idx].astype(int), y_pred[:,idx])
+    c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % (c_label, auc(fpr, tpr)))
+c_ax.legend()
+c_ax.set_xlabel('False Positive Rate')
+c_ax.set_ylabel('True Positive Rate')
+fig.savefig('trained_net.png')
+
+
+# In[27]:
+
+
+from sklearn.metrics import roc_auc_score
+roc_auc_score(test_Y.astype(int), y_pred)
+
+
+# In[ ]:
+
+
+
+
---
+++ Preprocessing/Xray Analysis Preprocessing.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Importing Necessary Libraries
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from glob import glob
+import os
+import matplotlib.gridspec as gridspec
+import matplotlib.ticker as ticker
+sns.set_style('whitegrid')
+get_ipython().run_line_magic('matplotlib', 'inline')
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# ### Data Loading 
+
+# In[2]:
+
+
+# reading the data
+data = pd.read_csv("C:\\Users\\Karan\\Downloads\\archive\\Data_Entry_2017.csv")
+data.head()
+
+
+# In[3]:
+
+
+print('shape of data : ',data.shape)
+
+
+# In[4]:
+
+
+data.describe()
+
+
+# ### Data cleaning
+
+# In[5]:
+
+
+#drop unused columns
+data = data[['Image Index','Finding Labels','Follow-up #','Patient ID','Patient Age','Patient Gender']]
+
+# removing the rows which have patient_age >100
+total = len(data)
+print('No. of rows before removing rows having age >100 : ',len(data))
+data = data[data['Patient Age']<100]
+print('No. of rows after removing rows having age >100 : ',len(data))
+print('No. of datapoints having age > 100 : ',total-len(data))
+
+
+# In[6]:
+
+
+# rows having no. of disease
+data['Labels_Count'] = data['Finding Labels'].apply(lambda text: len(text.split('|')) if(text != 'No Finding') else 0)
+
+
+# In[7]:
+
+
+label_counts = data['Finding Labels'].value_counts()[:15]
+fig, ax1 = plt.subplots(1,1,figsize = (12, 8))
+ax1.bar(np.arange(len(label_counts))+0.5, label_counts)
+ax1.set_xticks(np.arange(len(label_counts))+0.5)
+_ = ax1.set_xticklabels(label_counts.index, rotation = 90)
+
+
+# ### Data analysis 
+
+# ### Age distribution
+
+# In[15]:
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Assuming 'data' is your DataFrame
+sns.FacetGrid(data, hue='Patient Gender', height=5).map(sns.distplot, 'Patient Age').add_legend()
+plt.show()
+
+
+# #### Observation - Both the gender have almost same distribution
+
+# ### Disease distribution by age and sex 
+
+# In[16]:
+
+
+f, axarr = plt.subplots(7, 2, sharex=True,figsize=(15, 20))
+pathology_list = ['Cardiomegaly','Emphysema','Effusion','Hernia','Nodule','Pneumothorax','Atelectasis','Pleural_Thickening','Mass','Edema','Consolidation','Infiltration','Fibrosis','Pneumonia']
+df = data[data['Finding Labels'] != 'No Finding']
+i=0
+j=0
+x=np.arange(0,100,10)
+for pathology in pathology_list :
+    index = []
+    for k in range(len(df)):
+        if pathology in df.iloc[k]['Finding Labels']:
+            index.append(k)
+    g=sns.countplot(x='Patient Age', hue="Patient Gender",data=df.iloc[index], ax=axarr[i, j])
+    axarr[i, j].set_title(pathology)   
+    g.set_xlim(0,90)
+    g.set_xticks(x)
+    g.set_xticklabels(x)
+    j=(j+1)%2
+    if j==0:
+        i=(i+1)%7
+f.subplots_adjust(hspace=0.3)
+
+
+# ### No. of each disease by patient gender
+
+# In[17]:
+
+
+for pathology in pathology_list :
+    data[pathology] = data['Finding Labels'].apply(lambda x: 1 if pathology in x else 0)
+
+
+# In[18]:
+
+
+data.head()
+
+
+# In[19]:
+
+
+plt.figure(figsize=(15,10))
+gs = gridspec.GridSpec(8,1)
+ax1 = plt.subplot(gs[:7, :])
+ax2 = plt.subplot(gs[7, :])
+data1 = pd.melt(data,
+             id_vars=['Patient Gender'],
+             value_vars = list(pathology_list),
+             var_name = 'Category',
+             value_name = 'Count')
+data1 = data1.loc[data1.Count>0]
+g=sns.countplot(y='Category',hue='Patient Gender',data=data1, ax=ax1, order = data1['Category'].value_counts().index)
+ax1.set( ylabel="",xlabel="")
+ax1.legend(fontsize=20)
+ax1.set_title('X Ray partition (total number = 121120)',fontsize=18);
+
+data['Nothing']=data['Finding Labels'].apply(lambda x: 1 if 'No Finding' in x else 0)
+
+data2 = pd.melt(data,
+             id_vars=['Patient Gender'],
+             value_vars = list(['Nothing']),
+             var_name = 'Category',
+             value_name = 'Count')
+data2 = data2.loc[data2.Count>0]
+g=sns.countplot(y='Category',hue='Patient Gender',data=data2,ax=ax2)
+ax2.set( ylabel="",xlabel="Number of decease")
+ax2.legend('')
+plt.subplots_adjust(hspace=.5)
+
+
+# ### Ratio between one and multiple disease 
+
+# In[20]:
+
+
+df=data.groupby('Finding Labels').count().sort_values('Patient ID',ascending=False)
+df1=df[['|' in index for index in df.index]].copy()
+df2=df[['|' not in index for index in df.index]]
+df2=df2[['No Finding' not in index for index in df2.index]]
+df2['Finding Labels']=df2.index.values
+df1['Finding Labels']=df1.index.values
+
+
+# In[21]:
+
+
+f, ax = plt.subplots(sharex=True,figsize=(15, 10))
+sns.set_color_codes("pastel")
+g=sns.countplot(y='Category',data=data1, ax=ax, order = data1['Category'].value_counts().index,color='b',label="Multiple Pathologies")
+sns.set_color_codes("muted")
+g=sns.barplot(x='Patient ID',y='Finding Labels',data=df2, ax=ax, color="b",label="Simple Pathology")
+ax.legend(ncol=2, loc="center right", frameon=True,fontsize=20)
+ax.set( ylabel="",xlabel="Number of decease")
+ax.set_title("Comparaison between simple or multiple decease",fontsize=20)      
+sns.despine(left=True)
+
+
+# ### Plot most important pathologies groups for each desease
+
+# In[22]:
+
+
+#we just keep groups of pathologies which appear more than 30 times
+df3=df1.loc[df1['Patient ID']>30,['Patient ID','Finding Labels']]
+
+for pathology in pathology_list:
+    df3[pathology]=df3.apply(lambda x: x['Patient ID'] if pathology in x['Finding Labels'] else 0, axis=1)
+
+df3.head(20)
+
+
+# In[ ]:
+
+
+
+
